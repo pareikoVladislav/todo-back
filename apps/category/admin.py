b961d6a20d1e9dcf51951a7c17311d52400875d7
Vladislav Pareiko <parieiko.vlad@gmail.com>
2024-01-25 01:16:46 +0400

    feat: Register Category model to Admin Pannel

--- apps/category/admin.py
+++ apps/category/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.category.models import Category
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
