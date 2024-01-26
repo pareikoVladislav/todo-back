c80dd9ef0a024d55e9ddbe175919b1abebeb1ef6
Vladislav Pareiko <parieiko.vlad@gmail.com>
2024-01-26 15:01:23 +0400

    feat: Add new route to statuses CRUD

--- apps/router.py
+++ apps/router.py
@@ -3,4 +3,5 @@ from django.urls import include, path
 urlpatterns = [
     path('user/', include('apps.user.urls')),
     path('categories/', include('apps.category.urls')),
+    path('statuses/', include('apps.status.urls')),
 ]
