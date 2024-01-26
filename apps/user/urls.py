da671f1522ec216135d4029ac42ec45418f4a07e
Vladislav Pareiko <parieiko.vlad@gmail.com>
2024-01-26 22:15:22 +0400

    feat: Add new endpoints to CRUD user

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -5,12 +5,16 @@ from rest_framework_simplejwt.views import (
 
 from apps.user.views import (
     UserRegistrationGenericView,
+    ListUsersGenericView,
+    UserDetailGenericView,
 )
 from apps.jwt_config.views import (
     CustomTokenObtainPairView
 )
 
 urlpatterns = [
+    path("", ListUsersGenericView.as_view()),
+    path("<int:user_id>/", UserDetailGenericView.as_view()),
     path("register/", UserRegistrationGenericView.as_view()),
     path("auth/login/", CustomTokenObtainPairView.as_view()),
     path("auth/refresh-token/", TokenRefreshView.as_view()),
