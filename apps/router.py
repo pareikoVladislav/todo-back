fcbc357f36ef08fee91570c33ef3303d0d47b53e
Vladislav Pareiko <parieiko.vlad@gmail.com>
2024-01-26 22:14:33 +0400

    fix: UPdate user app routing

--- apps/router.py
+++ apps/router.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path('user/', include('apps.user.urls')),
+    path('users/', include('apps.user.urls')),
     path('categories/', include('apps.category.urls')),
     path('statuses/', include('apps.status.urls')),
     path('tasks/', include('apps.task.urls')),
