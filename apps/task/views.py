6f57a032184b82f4041e4dd8f4ca2ce0ab6a383d
Vladislav Pareiko <parieiko.vlad@gmail.com>
2024-01-26 16:14:31 +0400

    feat: Write new views to render all tasks and create new, and render specific task, and update, or delete this task

--- apps/task/views.py
+++ apps/task/views.py
@@ -1,3 +1,139 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.generics import (
+    get_object_or_404,
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
+from rest_framework import status
 
-# Create your views here.
+from apps.task.models import Task
+from apps.task.serializers import (
+    TaskInfoSerializer,
+    AllTasksSerializer
+)
+from apps.task.success_messages import (
+    TASK_UPDATED_SUCCESSFULLY_MESSAGE,
+    TASK_WAS_DELETED_SUCCESSFUL,
+    NEW_TASK_CREATED_MESSAGE,
+)
+
+
+class TasksListGenericView(ListCreateAPIView):
+    serializer_class = AllTasksSerializer
+
+    def get_queryset(self):
+        queryset = Task.objects.select_related(
+            'category',
+            'status'
+        ).prefetch_related('subtasks')
+
+        status_obj = self.request.query_params.get("status")
+        category = self.request.query_params.get("category")
+        date_from = self.request.query_params.get("date_from")
+        date_to = self.request.query_params.get("date_to")
+        deadline = self.request.query_params.get("deadline")
+
+        if status_obj:
+            queryset = queryset.filter(
+                status__name=status_obj
+            )
+        if category:
+            queryset = queryset.filter(
+                category__name=category
+            )
+        if date_from and date_to:
+            queryset = queryset.filter(
+                date_started__range=[date_from, date_to]
+            )
+        if deadline:
+            queryset = queryset.filter(
+                deadline=deadline
+            )
+
+        return queryset
+
+    def get(self, request: Request, *args, **kwargs):
+        filtered_data = self.get_queryset()
+
+        if filtered_data.exists():
+            serializer = self.serializer_class(
+                instance=filtered_data,
+                many=True
+            )
+
+            return Response(
+                status=status.HTTP_200_OK,
+                data=serializer.data
+            )
+
+        return Response(
+            status=status.HTTP_204_NO_CONTENT,
+            data=[]
+        )
+
+    def post(self, request: Request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(
+            status=status.HTTP_201_CREATED,
+            data={
+                "message": NEW_TASK_CREATED_MESSAGE,
+                "data": serializer.data
+            }
+        )
+
+
+class TaskDetailGenericView(RetrieveUpdateDestroyAPIView):
+    serializer_class = TaskInfoSerializer
+
+    def get_object(self):
+        task_id = self.kwargs.get("task_id")
+
+        task = get_object_or_404(Task, id=task_id)
+
+        return task
+
+    def get(self, request: Request, *args, **kwargs):
+        task = self.get_object()
+
+        serializer = self.serializer_class(task)
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data=serializer.data
+        )
+
+    def put(self, request: Request, *args, **kwargs):
+        task = self.get_object()
+
+        serializer = self.serializer_class(task, data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            return Response(
+                status=status.HTTP_200_OK,
+                data={
+                    "message": TASK_UPDATED_SUCCESSFULLY_MESSAGE,
+                    "data": serializer.data
+                }
+            )
+        else:
+            return Response(
+                status=status.HTTP_400_BAD_REQUEST,
+                data=serializer.errors
+            )
+
+    def delete(self, request, *args, **kwargs):
+        task = self.get_object()
+
+        task.delete()
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data=TASK_WAS_DELETED_SUCCESSFUL
+        )
