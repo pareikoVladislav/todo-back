46309524476a693d8ddf4833e240d6200dc6ae88
Vladislav Pareiko <parieiko.vlad@gmail.com>
2024-01-26 16:49:27 +0400

    feat: Register new model, add custom admin action

--- apps/subtask/admin.py
+++ apps/subtask/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.subtask.models import SubTask
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    def change_subtasks_register(self, request, queryset):
+        for obj in queryset:
+            obj.title = obj.title.upper()
+            obj.save()
+
+    change_subtasks_register.short_description = 'Up register'
+
+    actions = [
+        'change_subtasks_register',
+    ]
+
+    list_display = ('title', 'category', 'task', 'status', 'creator', 'created_at')
+    list_filter = ('task', 'category', 'status', 'creator', 'created_at')
+    search_fields = ('title',)
