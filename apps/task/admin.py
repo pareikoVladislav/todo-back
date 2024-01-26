7741b5a243d18fb3d4add6f3ead273a73af0da56
Vladislav Pareiko <parieiko.vlad@gmail.com>
2024-01-26 16:10:09 +0400

    feat: Register new Task model

--- apps/task/admin.py
+++ apps/task/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.task.models import Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'status', 'creator', 'created_at')
+    list_filter = ('category', 'status', 'creator', 'created_at')
+    search_fields = ('title',)
