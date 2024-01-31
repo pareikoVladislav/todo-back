35686a433012f71a588a45f359f4d7023915bad8
andrewdevelo <lemeshev.andrei@gmail.com>
2024-01-31 18:58:25 +0300

    feat: in class CategoryListGenericViewTest creat methods: test_post_category_valid_data and                test_post_category_invalid_data

--- apps/category/tests.py
+++ apps/category/tests.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import APIClient
-from apps.user.models import User
 from apps.category.models import Category
+from apps.user.models import User
+from rest_framework.test import APIClient
+from apps.category.success_messages import NEW_CATEGORY_CREATED_MESSAGE
 
 
 class CategoryListGenericViewTest(TestCase):
@@ -24,6 +25,7 @@ class CategoryListGenericViewTest(TestCase):
     def test_get_categories(self):
         self.client.force_authenticate(user=self.user)
         response = self.client.get(f'http://127.0.0.1:8000/api/v1/categories/')
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data['category'], str)
         self.assertEqual(Category.objects.count(), 1)
@@ -33,4 +35,28 @@ class CategoryListGenericViewTest(TestCase):
         self.client.force_authenticate(user=self.user)
         Category.objects.all().delete()
         response = self.client.get(f'http://127.0.0.1:8000/api/v1/categories/')
+
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_post_category_valid_data(self):
+        self.client.force_authenticate(user=self.user)
+        data = {
+            'name': 'Test Category'
+        }
+        response = self.client.post(f'http://127.0.0.1:8000/api/v1/categories/', data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['message'], NEW_CATEGORY_CREATED_MESSAGE)
+        self.assertEqual(response.data['data']['name'], 'Test Category')
+        self.assertTrue(Category.objects.filter(name='Test Category').exists())
+
+    def test_post_category_invalid_data(self):
+        self.client.force_authenticate(user=self.user)
+        data = {
+            'name': ''
+        }
+        response = self.client.post(f'http://127.0.0.1:8000/api/v1/categories/', data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn('name', response.data)
+        self.assertEqual(Category.objects.count(), 0)
